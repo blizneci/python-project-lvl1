bbf5e1886a49f13ff62e9b23c3a586935bf008d5
Kirill Zhelvakov <blzneci.89@gmail.com>
2023-05-18 02:37:53 +0300

    change cli.py, add return name

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -4,7 +4,8 @@ import prompt
 WELCOME_MESSAGE = "Welcome to the Brain Games!"
 
 
-def welcome_user(message=WELCOME_MESSAGE):
+def welcome_user(message: str = WELCOME_MESSAGE) -> str:
     print(message)
     name = prompt.string("May I have your name? ")
     print(f"Hello, {name}!")
+    return name
