940b97a084045caccfc63e9bf512854b1b3ce75a
Kirill Zhelvakov <blzneci.89@gmail.com>
2023-05-19 04:09:50 +0300

    change GAME_CATALOG constant

--- brain_games/storage.py
+++ brain_games/storage.py
@@ -1,16 +1,27 @@
+# storage.py
+
+"""
+
+This module implements a storage for games.
+
+"""
+
 from importlib import import_module
 
 
 GAMES_STORAGE_PATH = "brain_games.games"
 
-games_catalog = {
+GAMES_CATALOG = {
         "brain-even": "even",
         "brain-calc": "calc",
+        "brain-prime": "prime",
+        "brain-gcd": "gcd",
+        "brain-progression": "progression",
         }
 
 
 def get_game_location(script_name: str) -> str:
-    game_name = games_catalog.get(script_name)
+    game_name = GAMES_CATALOG.get(script_name)
     game_location = ".".join((GAMES_STORAGE_PATH, game_name))
     return game_location
 
