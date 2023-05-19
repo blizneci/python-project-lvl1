145c2c91349eee479a9814a44e4db230847f779f
Kirill Zhelvakov <blzneci.89@gmail.com>
2023-05-19 04:15:22 +0300

    change docstrings and imports

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,22 +1,17 @@
 #! /usr/bin/env python3
-import sys
-import pathlib
 
-from brain_games.cli import welcome_user
-from brain_games.storage import get_game_location, download_game
-from brain_games.engine import start_game
+"""
+
+This module gets game from storage and starts it.
+
+"""
+
+
+from brain_games import cli
 
 
 def main():
-    script_path = pathlib.PurePath(sys.argv[0])
-    script_name = script_path.name
-    if script_name == "brain-games":
-        welcome_user()
-        return
-    # for unified interface
-    game_location = get_game_location(script_name)
-    game = download_game(game_location)
-    start_game(game)
+    cli.welcome_user()
 
 
 if __name__ == "__main__":
